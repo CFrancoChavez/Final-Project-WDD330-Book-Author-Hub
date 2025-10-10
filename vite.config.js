f826717dea25989c169960e86da9303449b9661e
CFrancoChavez <franco.chvez@gmail.com>
2025-10-10 08:06:28 -0300

    changes added to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', 
   root: 'src',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+       
+      },
+    },
   },
   server: {
     port: 5173,
